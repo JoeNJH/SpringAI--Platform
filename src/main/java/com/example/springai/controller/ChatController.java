070daaef18cb0683a3ef889f43a51729745e52cf
wzunjh <wzunjh@qq.com>
2025-09-28 14:27:00 +1000

    多模态对话功能完成

--- src/main/java/com/example/springai/controller/ChatController.java
+++ src/main/java/com/example/springai/controller/ChatController.java
@@ -4,9 +4,16 @@ import com.example.springai.repository.ChatHistoryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.ai.chat.memory.ChatMemory;
+import org.springframework.ai.content.Media;
+import org.springframework.util.MimeType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import reactor.core.publisher.Flux;
+
+import java.util.List;
+import java.util.Objects;
 
 @RequiredArgsConstructor
 @RestController
@@ -18,19 +25,47 @@ public class ChatController {
     private final ChatHistoryRepository chatHistoryRepository;
 
     @RequestMapping(value = "/chat",produces = "text/html;charset=utf-8")
-    public Flux<String> chat(String prompt,String chatId){
+    public Flux<String> chat(@RequestParam("prompt") String prompt,
+                             @RequestParam("chatId") String chatId,
+                             @RequestParam("files") List<MultipartFile> files){
 
         // 1 存储会话ID
-
         chatHistoryRepository.save("chat",chatId);
 
-        // 2 请求模型
+        if (files == null || files.isEmpty() ){
+            // 没有文件上传 普通对话
+            return textChat(prompt,chatId);
+        }else {
+            return fileChat(prompt,chatId,files);
+        }
+    }
+
+    private Flux<String> fileChat(String prompt, String chatId, List<MultipartFile> files) {
+
+        // 转为媒体
+        List<Media> mediaList = files.stream()
+                .map(file
+                        -> new Media(MimeType.valueOf(Objects.requireNonNull(file.getContentType())),
+                        file.getResource())).toList();
+
+
+        return chatClient.prompt()
+              // 多媒体+提示词处理
+                .user(p -> p.text(prompt).media(mediaList.toArray(Media[]::new)))
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
+                .stream()
+                .content();
+
+    }
+
+    private Flux<String> textChat(String prompt, String chatId) {
 
         return chatClient.prompt()
                 .user(prompt)
                 .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
                 .stream()
                 .content();
+
     }
 
 }
