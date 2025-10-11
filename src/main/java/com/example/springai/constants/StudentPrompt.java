84283b7fc6ee60f75747fa1b01379f29b6950e51
wzunjh <wzunjh@qq.com>
2025-10-11 20:25:28 +1000

    新增agent学生模拟

---
+++ src/main/java/com/example/springai/constants/StudentPrompt.java
@@ -0,0 +1,56 @@
+package com.example.springai.constants;
+
+public class StudentPrompt {
+
+    public static final String PROMPT1 = """
+       
+       
+       
+
+
+
+
+
+
+       """;
+
+
+    public static final String PROMPT2 = """
+       
+       
+       
+
+
+
+
+
+
+       """;
+
+
+    public static final String PROMPT3 = """
+       
+       
+       
+
+
+
+
+
+
+       """;
+
+
+    public static final String PROMPT4 = """
+       
+       
+       
+
+
+
+
+
+
+       """;
+
+}
--- src/main/java/com/example/springai/controller/AgentController.java
+++ src/main/java/com/example/springai/controller/AgentController.java
@@ -18,19 +18,20 @@ public class AgentController {
 
     private final ChatClient Agent1ChatClient;
 
+    private final ChatClient Agent2ChatClient;
+
+    private final ChatClient Agent3ChatClient;
+
+    private final ChatClient Agent4ChatClient;
+
     private final ChatHistoryRepository chatHistoryRepository;
 
     @RequestMapping(value = "/chat/1",produces = "text/html;charset=utf-8")
-    public Flux<String> chat(@RequestParam("prompt") String prompt,
+    public Flux<String> chat1(@RequestParam("prompt") String prompt,
                              @RequestParam("chatId") String chatId){
 
         // 1 存储会话ID
         chatHistoryRepository.save("agent",chatId);
-
-        return textChat(prompt,chatId);
-    }
-
-    private Flux<String> textChat(String prompt, String chatId) {
 
         return Agent1ChatClient.prompt()
                 .user(prompt)
@@ -39,5 +40,53 @@ public class AgentController {
                 .content();
 
     }
+
+    @RequestMapping(value = "/chat/2",produces = "text/html;charset=utf-8")
+    public Flux<String> chat2(@RequestParam("prompt") String prompt,
+                             @RequestParam("chatId") String chatId){
+
+        // 1 存储会话ID
+        chatHistoryRepository.save("agent",chatId);
+
+        return Agent2ChatClient.prompt()
+                .user(prompt)
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
+                .stream()
+                .content();
+
+    }
+
+    @RequestMapping(value = "/chat/3",produces = "text/html;charset=utf-8")
+    public Flux<String> chat3(@RequestParam("prompt") String prompt,
+                             @RequestParam("chatId") String chatId){
+
+        // 1 存储会话ID
+        chatHistoryRepository.save("agent",chatId);
+
+        return Agent3ChatClient.prompt()
+                .user(prompt)
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
+                .stream()
+                .content();
+
+    }
+
+
+    @RequestMapping(value = "/chat/4",produces = "text/html;charset=utf-8")
+    public Flux<String> chat(@RequestParam("prompt") String prompt,
+                             @RequestParam("chatId") String chatId){
+
+        // 1 存储会话ID
+        chatHistoryRepository.save("agent",chatId);
+
+        return Agent4ChatClient.prompt()
+                .user(prompt)
+                .advisors(a->a.param(ChatMemory.CONVERSATION_ID,chatId))
+                .stream()
+                .content();
+
+    }
+
+
 
 }
